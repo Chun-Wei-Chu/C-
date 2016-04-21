f2819a1106c8e90d9a283208f89cd3d789903aa9
Chun-Wei-Chu <max2916@gmail.com>
2016-04-21 17:44:14 +0800

    smart point

--- SmartPoint/main.cpp
+++ SmartPoint/main.cpp
@@ -14,6 +14,12 @@ int main()
   memcpy(&(tmp.get())[0], "hello world\n",11);
   memcpy(&(tmp.get())[20], "hello world\n",11);
   testPtr(tmp.get());
+  
+  ---------------------------------
+  shared_ptr<int> sp(new int); 
+  *sp = 100000;
+  int* b;
+  b = sp.get();
 
   return 0;
 }
