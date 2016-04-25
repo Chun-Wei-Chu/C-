01c8d474d319eb4e3bcdf0cdc063380e4661672d
Chun-Wei-Chu <max2916@gmail.com>
2016-04-25 10:19:20 +0800

    template class example

--- template/class.cpp
+++ template/class.cpp
@@ -14,8 +14,25 @@
 	 }
  };
  
- /*-------------------------------------------------------*/
+ /*---------------------------------------------*/
  
  testTmp<int, int> hello;
 	hello.setTmp(1);
 	cout << hello.getTmp() <<endl;
+
+/***********************************************************************/
+template <class T, class V>
+class testTmp
+{
+public:
+	template<class A, class B> 
+	void test(A a, B b)
+	{
+		cout << b << endl;
+	}
+};
+
+/*----------------------------------------------*/
+
+testTmp<char, char> hello;
+hello.test<int, int>(1, 2);
